01fd25f7ce44a31d54664a3c606258624be23eda
Man Leung <lepusleung@gmail.com>
2017-09-20 18:37:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-function theBeatlesPlay(players, instruments) {
-    var allPlayers = [];
-    for (var i=0; i < players.length; i++) {
-        allPlayers.push(players[i] + " plays " + instruments[i]);
-    }
-    return allPlayers;
-}
-
-function johnLennonFacts() {
+function theBeatlesPlay(musicians, instruments) {
   var array = [];
-  var i = 0;
-  while (i < facts.length) {
-    array.push(facts[i] + "!!!");
-    i++;
+  for (var i = 0; i < musicians.length; i++) {
+  	array.push(musicians[i] + ' plays ' + instruments[i]);
   }
   return array;
 }
 
-function iLoveTheBeatles(num) {
-    var array = [];
-    do {
-        array.push("I love the Beatles!");
-        num++;
-    } while (num < 15);
-    return array;
+function johnLennonFacts(facts) {
+  var results = [];
+  var count = 0;
+  while (count < facts.length) {
+    results.push(facts[count] + "!!!");
+    count++;
   }
+  return results;
+}
+
+function iLoveTheBeatles(num) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!")
+    num++;
+  } while (num < 15);
+  return array;
+}
