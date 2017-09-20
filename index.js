0c2c3e56c73ca0d784ef7ba2ee5423e9beb015e0
Man Leung <lepusleung@gmail.com>
2017-09-20 18:33:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,10 +8,8 @@ function theBeatlesPlay(players, instruments) {
 
 function johnLennonFacts() {
   var facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
+  "foo!!!",
+  "bar!!!"
 ];
   var array = [];
   var i = 0;
